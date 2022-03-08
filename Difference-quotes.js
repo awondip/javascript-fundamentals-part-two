8351002536159006e80b35035394a84126e3db2d
dylane <awondipdylane047@.com>
2022-03-08 16:01:51 +0100

    Explaining the diffrence in quotes in terms of string

---
+++ Difference-quotes.js
@@ -0,0 +1,8 @@
+// A string in JavaScript must be surrounded by quotes.
+//In JavaScript, there are 3 types of quotes.
+
+// Double quotes: "Hello".
+// Single quotes: 'Hello'.
+// Backticks: `Hello`.
+
+
