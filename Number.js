ddace2fdf7edf4f4f98056034fdab6471f795912
dylane <awondipdylane047@.com>
2022-03-08 12:35:06 +0100

    Numbers Data types Explanation

---
+++ Number.js
@@ -0,0 +1,15 @@
+// let n = 123;
+// n = 12.345;
+
+// The number type represents both integer and floating point numbers.
+
+// There are many operations for numbers, e.g. multiplication *, division /, addition +, subtraction -, and so on.
+
+// Besides regular numbers, there are so-called “special numeric values” which also belong to this data type: Infinity, -Infinity and NaN.
+
+// Infinity represents the mathematical Infinity ∞. It is a special value that’s greater than any number.
+
+// We can get it as a result of division by zero:
+// example
+let division = (1/0)
+console.log(division)
