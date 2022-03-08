51968fad953e1234d7a99743b09d817dc466858f
dylane <awondipdylane047@.com>
2022-03-08 14:20:10 +0100

    Explanation of Object and Symbols Data-types

---
+++ Object-and-Symbols.js
@@ -0,0 +1,8 @@
+//objects are used to store keyed collections of various data and more complex entities. 
+//An object can be created with figure brackets {…} with an optional list of properties.
+//example
+let user = {     // an object
+    name: "John",  // by key "name" store value "John"
+    age: 30        // by key "age" store value 30
+  };
+  //The symbol type is used to create unique identifiers for objects. We have to mention it here for the sake of completeness, but also postpone the details till we know objects.
