ad33c110bae2f1d951ab51c8740f3866243ff6f3
dylane <awondipdylane047@.com>
2022-03-08 13:34:07 +0100

    Expplainig BooleanBoolean.js

---
+++ Boolean.js
@@ -0,0 +1,11 @@
+// The boolean type has only two values: true and false.
+
+// This type is commonly used to store yes/no values: true means “yes, correct”, and false means “no, incorrect”.
+
+// For instance:
+
+// let nameFieldChecked = true; // yes, name field is checked
+// let ageFieldChecked = false; // no, age field is not checked
+
+let isGreater = 4 > 1;
+console.log(isGreater) 
