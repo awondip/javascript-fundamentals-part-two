5ed2d5cb091d3ef15abc61b6ed5bb808a59ef9c6
dylane <awondipdylane047@.com>
2022-03-08 14:36:44 +0100

    Explanation of the typeof operator

---
+++ typeof-operator.js
@@ -0,0 +1,13 @@
+// The typeof operator returns the type of the argument. It’s useful when we want to process values of different types differently or just want to do a quick check.
+
+// A call to typeof x returns a string with the type name:
+
+console.log (typeof undefined) 
+
+console.log (typeof 0) 
+
+console.log (typeof 10n)
+
+console.log (typeof true)
+
+console.log (typeof "foo")
