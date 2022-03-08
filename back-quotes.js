392696c998c4044988b2e7d1ac09e8f151b799d6
dylane <awondipdylane047@.com>
2022-03-08 16:21:40 +0100

    Expalining backquotes with example

---
+++ back-quotes.js
@@ -0,0 +1,9 @@
+//Backticks are “extended functionality” quotes. They allow us to embed variables and expressions into a string by wrapping them in ${…}, for example:
+
+let name = "John";
+
+// embed a variable
+console.log( `Hello, ${name}!` ); 
+
+// embed an expression
+console.log( `the result is ${1 + 2}` ); 
