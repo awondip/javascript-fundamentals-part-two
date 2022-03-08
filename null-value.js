bf74a9aa6061195b7a382d14edb2ca2990be9a07
dylane <awondipdylane047@.com>
2022-03-08 13:54:11 +0100

    Exlpaning null-value

---
+++ null-value.js
@@ -0,0 +1,6 @@
+// In JavaScript, null is not a “reference to a non-existing object” or a “null pointer” like in some other languages.
+
+// It’s just a special value which represents “nothing”, “empty” or “value unknown”
+//example
+let age = null;
+console.log(age)
