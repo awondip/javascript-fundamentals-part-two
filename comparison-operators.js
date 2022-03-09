a83230c47f2c64edbfb846e7b59e342b8fa32702
dylane <awondipdylane047@.com>
2022-03-09 11:38:12 +0100

    Explaining the  comparison operator

---
+++ comparison-operators.js
@@ -0,0 +1,54 @@
+// We know many comparison operators from maths.
+
+// In JavaScript they are written like this:
+
+// Greater/less than: a > b, a < b.
+// Greater/less than or equals: a >= b, a <= b.
+// Equals: a == b, please note the double equality sign == means the equality test, while a single one a = b means an assignment.
+// Not equals: In maths the notation is ≠, but in JavaScript it’s written as a != b
+
+// All comparison operators return a boolean value:
+
+// true – means “yes”, “correct” or “the truth”.
+// false – means “no”, “wrong” or “not the truth”.
+// For example:
+
+console.log( 2 > 1 );  // true (correct)
+console.log( 2 == 1 ); // false (wrong)
+console.log( 2 != 1 ); // true (correct)
+//A comparison result can be assigned to a variable, just like any value:
+
+let result = 5 > 4; // assign the result of the comparison
+console.log( result ); // true
+
+// To see whether a string is greater than another, JavaScript uses the so-called “dictionary” or “lexicographical” order.
+
+// In other words, strings are compared letter-by-letter.
+
+// For example:
+
+console.log( 'Z' > 'A' ); // true
+console.log( 'Glow' > 'Glee' ); // true
+console.log( 'Bee' > 'Be' ); // true
+
+// The algorithm to compare two strings is simple:
+
+// Compare the first character of both strings.
+// If the first character from the first string is greater (or less) than the other string’s, then the first string is greater (or less) than the second. We’re done.
+// Otherwise, if both strings’ first characters are the same, compare the second characters the same way.
+// Repeat until the end of either string.
+// If both strings end at the same length, then they are equal. Otherwise, the longer string is greater.
+
+// When comparing values of different types, JavaScript converts the values to numbers.
+
+// For example:
+
+console.log( '2' > 1 ); // true, string '2' becomes a number 2
+console.log( '01' == 1 ); // true, string '01' becomes a number 1
+//For boolean values, true becomes 1 and false becomes 0.
+
+//The value undefined shouldn’t be compared to other values:
+
+console.log( undefined > 0 ); // false (1)
+console.log( undefined < 0 ); // false (2)
+console.log( undefined == 0 ); // false (3)
