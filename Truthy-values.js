26b2950276e09527d5befc0536a682465478fd81
dylane <awondipdylane047@.com>
2022-03-09 17:22:26 +0100

    Naming truthy-values

---
+++ Truthy-values.js
@@ -0,0 +1,11 @@
+//Truthy values In JavaScript, a truthy value is a value that is considered true when encountered in a Boolean context.
+//The following values are considered falsy:
+
+false
+0
+-0
+""
+null
+undefined
+NaN
+//All other values are considered truthy!
